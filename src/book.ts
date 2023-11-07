ee55cea6aaed51b20615a0cb9c1e4170349202c0
Tabrha <tesfayeabrha95@gmail.com>
2023-11-06 23:13:49 -0600

    new changes

--- src/book.ts
+++ src/book.ts
@@ -96,4 +96,82 @@
 //     console.log("implement scramble if you have time ...");
 
 // }
+/******************************************************************* */
+// Define the Book type
+type Book = {
+    title: string;
+    author: string;
+    libraryID: number;
+  };
+  
+  // Array to store the library books
+  const libraryBooks: Book[] = [];
+  
+  // Function to create a new book and add it to the libraryBooks array
+  function createBook(title: string, author: string, libraryID: number): Book {
+    const newBook: Book = { title, author, libraryID };
+    libraryBooks.push(newBook);
+    return newBook;
+  }
+  
+  // Function to find and return all book titles, sorted alphabetically
+  function findTitles(): string[] {
+    return libraryBooks.map((book) => book.title).sort();
+  }
+  
+  // Function to find and return all authors, sorted alphabetically
+  function findAuthors(): string[] {
+    return libraryBooks.map((book) => book.author).sort();
+  }
+  
+  // Function to find and return all libraryIDs, sorted alphabetically
+  function findIDs(): number[] {
+    return libraryBooks.map((book) => book.libraryID).sort();
+  }
+  
+  // Handle button clicks and display the result in the textarea
+  function handleButtonClick(event: Event): void {
+    const button = event.target as HTMLButtonElement;
+    const action = button.value;
+  
+    let result: string[] = [];
+    if (action === 'Titles') {
+      result = findTitles();
+    } else if (action === 'Authors') {
+      result = findAuthors();
+    } else if (action === 'libraryID') {
+      result = findIDs().map((id) => id.toString());
+    }
+  
+    const textarea = document.getElementById('result') as HTMLTextAreaElement;
+    textarea.value = result.join('\n');
+  }
+  
+  // Attach click event handlers to the buttons
+  document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+      button.addEventListener('click', handleButtonClick);
+    });
+  });
+  
 
+
+
+// export function findTitles(): string[] {
+//     return library.map((book) => book.title).sort();
+//   }
+  
+//   export function addBook(title: string, author: string, libraryID: number): void {
+//     const newBook: Book = { title, author, libraryID };
+//     library.push(newBook);
+//   }
+  
+//   export function findAuthors(): string[] {
+//     return library.map((book) => book.author).sort();
+//   }
+  
+//   export function findIDs(): number[] {
+//     return library.map((book) => book.libraryID).sort((a, b) => a - b);
+//   }
+  
